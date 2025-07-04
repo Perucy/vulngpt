8b2961c43a1db79aecc248a7a3f924b49a10ed81
Perucy Mussiba <perucymussiba@gmail.com>
2025-07-03 23:30:46 -0700

    Update astra_database.py

--- astra_database.py
+++ astra_database.py
@@ -1,6 +1,5 @@
 import os
 from dotenv import load_dotenv
-#from astrapy.info import CollectionVectorServiceOptions
 from astrapy import DataAPIClient
 from astrapy.constants import VectorMetric
 from langchain_openai import OpenAIEmbeddings
